fca2099f04c22dc7830e2dbbd88ed3f6511205d2
Ahshanul Haider Tanha <61645636+AhshanulHaiderTAnha@users.noreply.github.com>
2023-12-11 14:01:55 +0600

    code update

--- views/delete-button.blade.php
+++
@@ -1,5 +0,0 @@
-<delete-button inline-template
-               :page="{{json_encode($page)}}"
->
-    <button @click="deletePage()" type="button" class="btn btn-danger"><i class="fa fa-trash-o"></i></button>
-</delete-button>
